62342afc3a72f27bbce17bc59b4287e1889cda9d
Kael <i@kael.me>
2016-01-28 15:57:29 +0800

    fixes $(html).width()

--- src/zepto.js
+++ src/zepto.js
@@ -682,7 +682,7 @@ var Zepto = (function() {
         $this.css(props)
       })
       if (!this.length) return null
-      if (!$.contains(document.documentElement, this[0]))
+      if (!$.contains(document.documentElement, this[0]) && document.documentElement !== this[0])
         return {top: 0, left: 0}
       var obj = this[0].getBoundingClientRect()
       return {
